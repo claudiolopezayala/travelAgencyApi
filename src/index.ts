cf88915bf090c6773456df5d0946d9ce1b040664
claudiwicode <claudio.wicode@gmail.com>
2024-11-24 20:29:07 -0600

    add db connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { envs } from "./utils/dotenv"
 import appRouter from "./router/app-router"
+import { MongoDatabase } from "./db/connection";
 
-const app = express()
+const app = express();
 
+(async () => await MongoDatabase.connect({mongoUrl: envs.MONGO_URL, dbName: envs.MONGO_DB}))();
 app.use("/", appRouter)
 
 app.listen(envs.PORT, ()=>{
--- src/utils/dotenv.ts
+++ src/utils/dotenv.ts
@@ -3,4 +3,6 @@ import env from "env-var"
 
 export const envs ={
   PORT: env.get("PORT").required().asPortNumber(),
+  MONGO_URL: env.get("MONGO_URL").required().asString(),
+  MONGO_DB: env.get("MONGO_DB").required().asString(),
 }
