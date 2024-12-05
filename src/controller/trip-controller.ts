c8a974b72080d643f5a8e0c1da2ba2814f52df90
claudiwicode <claudio.wicode@gmail.com>
2024-12-04 18:16:56 -0600

    add trip swagger

--- src/controller/trip-controller.ts
+++ src/controller/trip-controller.ts
@@ -2,6 +2,31 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { UserModel } from "../model/user-model";
 
+/**
+ * @swagger
+ * /trip/{id_user}:
+ *   post:
+ *     summary: Obtener los viajes de un usuario
+ *     tags:
+ *       - Viajes
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: id_user
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *           pattern: '^[0-9a-fA-F]{24}$'
+ *         description: ID del usuario
+ *     responses:
+ *       200:
+ *         description: Lista de viajes del usuario
+ *       400:
+ *         description: Solicitud incorrecta
+ *       401:
+ *         description: No autenticado
+ */
 export const getUsersTrip = async (req: Request, res: Response) : Promise<any> => {
   try {
     const { id_user } = req.params;
@@ -20,6 +45,58 @@ export const getUsersTrip = async (req: Request, res: Response) : Promise<any> =
   }
 };
 
+/**
+ * @swagger
+ * /trip/:
+ *   post:
+ *     summary: Crear un nuevo viaje
+ *     tags:
+ *       - Viajes
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - id_user
+ *               - trip
+ *             properties:
+ *               id_user:
+ *                 type: string
+ *                 pattern: '^[0-9a-fA-F]{24}$'
+ *               trip:
+ *                 type: object
+ *                 required:
+ *                   - place
+ *                   - description
+ *                   - lat
+ *                   - lng
+ *                 properties:
+ *                   place:
+ *                     type: string
+ *                     maxLength: 150
+ *                   description:
+ *                     type: string
+ *                     maxLength: 500
+ *                   lat:
+ *                     type: number
+ *                     minimum: -90
+ *                     maximum: 90
+ *                   lng:
+ *                     type: number
+ *                     minimum: -180
+ *                     maximum: 180
+ *     responses:
+ *       201:
+ *         description: Viaje creado exitosamente
+ *       400:
+ *         description: Solicitud incorrecta
+ *       401:
+ *         description: No autenticado
+ */
 export const createTrip = async (req: Request, res: Response) : Promise<any> => {
   try {
     const {id_user, trip} = req.body
